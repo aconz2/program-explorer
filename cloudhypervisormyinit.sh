781b0c84d0e5f706cc6bf2c579f329d1092b1e08
Andrew <aconz2@gmail.com>
2024-07-23 18:01:37 -0500

    yeah it works

--- cloudhypervisormyinit.sh
+++ cloudhypervisormyinit.sh
@@ -5,6 +5,7 @@ k=/home/andrew/Repos/linux/vmlinux
     --kernel $k \
     --initramfs init1.initramfs \
     --cmdline "console=hvc0 reboot=t" \
+    --disk path=gcc-squashfs.sqfs,readonly=on \
     --cpus boot=1 \
     --memory size=1024M \
     --vsock cid=3,socket=/tmp/ch.sock
