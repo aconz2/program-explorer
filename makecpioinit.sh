3c90259447b1da772cf811a829e7211fa5a7f7a0
Andrew <aconz2@gmail.com>
2024-07-24 13:10:34 -0500

    make ext4 from initramfs

--- makecpioinit.sh
+++ makecpioinit.sh
@@ -1,1 +1,8 @@
 ~/Repos/linux/usr/gen_init_cpio initattempt1 > init1.initramfs
+size=$(stat --format='%s' init1.initramfs)
+size=$(($size / 1024 / 1024 + 10))
+tmpdir=$(mktemp -d)
+trap "sudo rm -rf $tmpdir" EXIT
+# need sudo with mknod
+sudo cpio -v --extract --directory $tmpdir < init1.initramfs
+sudo mkfs.ext4 -d $tmpdir init1-ext4.img "${size}m"
