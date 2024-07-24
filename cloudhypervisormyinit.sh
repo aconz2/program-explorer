5df4d14893b560e589ca1ff03b01041078b79b8b
Andrew <aconz2@gmail.com>
2024-07-24 13:19:49 -0500

    cloud hypervisor doesn't support --disk multiple times

--- cloudhypervisormyinit.sh
+++ cloudhypervisormyinit.sh
@@ -5,7 +5,8 @@ k=/home/andrew/Repos/linux/vmlinux
     --kernel $k \
     --initramfs init1.initramfs \
     --cmdline "console=hvc0 reboot=t" \
-    --disk path=gcc-squashfs.sqfs,readonly=on \
+    --disk path=init1-ext4.img,readonly=on,id=root \
+    --disk path=gcc-squashfs.sqfs,readonly=on,id=container-bundle-squashfs \
     --cpus boot=1 \
     --memory size=1024M \
     --vsock cid=3,socket=/tmp/ch.sock
