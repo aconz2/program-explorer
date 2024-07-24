b829233fa0f5645db49fa7f8630ca256fc05a5c1
Andrew <aconz2@gmail.com>
2024-07-24 13:38:12 -0500

    got things to run

--- cloudhypervisormyinit.sh
+++ cloudhypervisormyinit.sh
@@ -5,7 +5,6 @@ k=/home/andrew/Repos/linux/vmlinux
     --kernel $k \
     --initramfs init1.initramfs \
     --cmdline "console=hvc0 reboot=t" \
-    --disk path=init1-ext4.img,readonly=on,id=root \
     --disk path=gcc-squashfs.sqfs,readonly=on,id=container-bundle-squashfs \
     --cpus boot=1 \
     --memory size=1024M \
