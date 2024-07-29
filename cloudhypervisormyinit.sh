8eac9782074da5bd98805c8bae795de5c28620ce
Andrew <aconz2@gmail.com>
2024-07-29 16:05:35 -0500

    checking in

--- cloudhypervisormyinit.sh
+++ cloudhypervisormyinit.sh
@@ -1,11 +1,12 @@
 k=/home/andrew/Repos/linux/vmlinux
 
 # this worked
-./cloud-hypervisor-static \
+exec ./cloud-hypervisor-static \
     --kernel $k \
     --initramfs init1.initramfs \
+    --debug-console file=/tmp/chdc \
     --cmdline "console=hvc0" \
     --disk path=gcc-squashfs.sqfs,readonly=on,id=container-bundle-squashfs \
     --cpus boot=1 \
     --memory size=1024M \
-    --vsock cid=3,socket=/tmp/ch.sock
+    --vsock cid=3,socket=/tmp/ch.sock $@
