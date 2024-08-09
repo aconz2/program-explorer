b79b9fbf1036ceb6089ab27d8811ea139c4068ef
Andrew Consroe <aconz2@gmail.com>
2024-08-09 16:55:26 -0500

    stuff

--- cloudhypervisormyinit.sh
+++ cloudhypervisormyinit.sh
@@ -3,8 +3,6 @@ k=/home/andrew/Repos/linux/vmlinux
 # exit vsockhello procs when we ctrl-c
 trap "pkill -P $$" EXIT
 
-    #--console off \
-
 #strace --decode-pids=comm --trace=!ioctl,close,mmap,munmap,io_uring_enter -f -o chstrace.out ./cloud-hypervisor-static \
 
 rm -f /tmp/ch.sock*
@@ -18,17 +16,16 @@ echo 'hi' > /tmp/_stdin
 ./vsockhello u/tmp/ch.sock_124 0 cpio -i -D /tmp/_out &
 #./vsockhello u/tmp/ch.sock_124 0 cat > /tmp/_out.cpio &
 
-    #--pmem file=gcc-14.1.0.sqfs,discard_writes=on \
+    #--disk path=gcc-14.1.0.sqfs,readonly=on,id=gcc14 \
 #strace --decode-pids=comm -f ./cloud-hypervisor-static \
 time ./cloud-hypervisor-static \
     --kernel $k \
     --initramfs initramfs \
     --serial off \
-    --pmem file=pmemtestfile \
-    --disk path=gcc-14.1.0.sqfs,readonly=on,id=gcc14 \
-    --cmdline "initcall_debug console=hvc0" \
+    --pmem file=gcc-14.1.0.sqfs,discard_writes=on file=pmemtestfile \
+    --cmdline "console=hvc0" \
     --cpus boot=1 \
-    --memory size=1024M \
+    --memory size=1024M,thp=on \
     --vsock cid=3,socket=/tmp/ch.sock $@
 
 #wait
