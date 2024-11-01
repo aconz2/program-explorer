d1b7643dfbb67ee55f86e0ab136dbd07d2440b8d
Andrew Consroe <aconz2@gmail.com>
2024-11-01 17:54:27 -0500

    tokio stuff

--- peserver/src/main.rs
+++ peserver/src/main.rs
@@ -1,21 +1,28 @@
-use std::io;
-use std::io::{Read,Write};
+//use std::io;
+//use std::io::{Read,Write};
+use std::io::Cursor;
 
-use mio;
-use mio::{Events, Interest, Poll, Registry};
+use tokio::io::{AsyncReadExt,AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+//use mio;
+//use mio::{Events, Interest, Poll, Registry};
 use std::net::SocketAddr;
-use mio::net::{TcpListener,TcpStream};
+//use mio::net::{TcpListener,TcpStream};
 use std::time::Duration;
 use clap::{Parser};
-use tempfile::NamedTempFile;
+//use tempfile::NamedTempFile;
 use httparse;
 use atoi_simd;
+use byteorder::{ReadBytesExt,LE};
+use serde::{Serialize,Deserialize};
 
-mod mytimerfd;
-use mytimerfd::TimerFd;
+// mod mytimerfd;
+// use mytimerfd::TimerFd;
 
 const MAX_HEADER_SIZE_BYTES: usize = 4096;
 const MAX_HEADER_COUNT: usize = 5;
+const MAX_CONFIG_SIZE_BYTES: usize = 4096;
 // max body size caps the size of our io file, though we have the config header additionally and it
 // will get truncated up to nearest 2 MB alignment, so really it should be about MAX_BODY_SIZE +
 // 2MB since the config should always fit in 2 MB anyways
@@ -27,302 +34,128 @@ struct Config {
     timeout_per_conn: Duration,
 }
 
-enum State {
-    ReadingHeaders,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ApiV1Data {
+    pub image: String,
 }
 
-enum Token {
-    Server,
-    RequestConnection(usize),
-    RequestTimer(usize),
+async fn send_response(mut socket: TcpStream, status: usize, message: &str) {
+    let l = message.as_bytes().len();
+    let s = format!("HTTP/1.1 {status}\r\nContent-length: {l}\r\n\r\n{message}");
+    let data = s.as_str().as_bytes();
+    socket.write_all(&data).await.unwrap();
 }
 
-impl Token {
-    fn connection(index: usize) -> Self {
-        Self::RequestConnection(index)
-    }
-    fn timer(index: usize) -> Self {
-        Self::RequestTimer(index)
-    }
-}
+async fn process_api_v1_i(mut socket: TcpStream, content_length: usize, mut body_start: Vec<u8>) {
+    if content_length < 4 { return send_response(socket, 400, "body too short").await; }
 
-impl From<mio::Token> for Token {
-    fn from(mio::Token(x): mio::Token) -> Token {
-        if x == 0 { return Token::Server; }
-        let x = x - 1;
-        let (index, k) = (x / 2, x % 2);
-        match k {
-            0 => Token::RequestConnection(index),
-            1 => Token::RequestTimer(index),
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl Into<mio::Token> for Token {
-    fn into(self) -> mio::Token {
-        let i = match self {
-            Token::Server => 0,
-            Token::RequestConnection(i) => i * 2 + 1,
-            Token::RequestTimer(i)      => i * 2 + 2,
-        };
-        mio::Token(i)
-    }
-}
-
-struct RequestResources {
-    index: usize,
-    buf_offset: usize,
-    buf: Box<[u8]>,
-    timer: TimerFd,
-    io_file: NamedTempFile,
-}
-
-impl RequestResources {
-    fn new(index: usize) -> io::Result<Self> {
-        let timer = TimerFd::new()?;
-        Ok(Self {
-            index: index,
-            buf: Box::new([0; MAX_HEADER_SIZE_BYTES]),
-            buf_offset: 0,
-            timer: timer,
-            io_file: NamedTempFile::new()?,
-        })
-    }
-
-    fn into_request(self, conn_addr: (TcpStream, SocketAddr)) -> Request {
-        Request::new(self, conn_addr)
-    }
-
-    fn reset(mut self) -> Self {
-        self.buf_offset = 0;
-        self.timer.unset().expect("error resetting timer"); // idk propagate?
-        // self.buf.clear();
-        self.io_file.as_file().set_len(0).expect("error truncating");
-        self
-    }
-}
-
-struct Request {
-    state: State,
-    resources: RequestResources,
-    address: SocketAddr,
-    connection: TcpStream,
-}
-
-fn escape_dump(input: &[u8]) {
-    use std::io::Write;
-    let mut output = Vec::<u8>::with_capacity(1024);
-    for b in input {
-        for e in std::ascii::escape_default(*b) {
-            output.push(e);
-        }
-    }
-    let _ = io::stdout().write_all(output.as_slice()).unwrap();
-    println!("");
-}
-
-impl Request {
-    fn new(resources: RequestResources, (connection, address): (TcpStream, SocketAddr)) -> Self {
-        Self {
-            state: State::ReadingHeaders,
-            resources: resources,
-            address: address,
-            connection: connection,
-        }
-    }
-
-    fn index(&self) -> usize { self.resources.index }
-
-    fn into_resources(mut self, registry: &Registry) -> RequestResources {
-        let _ = self.deregister(registry);
-        self.resources.reset()
-    }
-
-    fn connection_ready(&mut self) -> io::Result<()> {
-        // we are going to have to transition to registering for writable, so we need the
-        // registry here
-        match self.state {
-            State::ReadingHeaders => {
-                if self.resources.buf_offset + 1 == self.resources.buf.len() {
-                    panic!("guaranteed by check in partial read");
-                }
-                let n = self.connection.read(&mut self.resources.buf[self.resources.buf_offset..])?;
-                if n == 0 {
-                    todo!("eof, send error response and close");
-                }
-                let data = &self.resources.buf[..self.resources.buf_offset + n];
-                self.resources.buf_offset = data.len();
-                {
-                    escape_dump(&data);
-                }
-                // I tried for a second to move these into RequestResources but the lifetimes get a
-                // bit hairy
-                let mut headers = [httparse::EMPTY_HEADER; MAX_HEADER_COUNT];
-                let mut request = httparse::Request::new(&mut headers[..]);
-                let res = request.parse(data);
-                match res {
-                    Ok(httparse::Status::Complete(body_start)) => {
-                        eprintln!("yo lets go method={:?} path={:?} version={:?}", request.method, request.path, request.version);
-                        let body = &data[body_start..];
-                        match content_length(&request) {
-                            Some(body_length) => {
-                                if body_length as usize > MAX_BODY_SIZE {
-                                    todo!("body size too big, send error response and close");
-                                } else if body.len() == body_length as usize {
-                                    todo!("complete body");
-                                    // tiny write, just do it
-                                    match self.resources.io_file.write_all(body) {
-                                        Ok(_) => {
-                                            todo!("ready to put me on the queue");
-                                        },
-                                        Err(_) => {
-                                            todo!("got error writing body");
-                                        }
-                                    }
-                                } else {
-                                    todo!("incomplete body, move to ReadingBody state");
-                                }
-                            },
-                            None => {
-                                todo!("no content length, send error and close");
-                            }
-                        }
-                    }
-                    Ok(httparse::Status::Partial) => {
-                        if self.resources.buf_offset == MAX_HEADER_SIZE_BYTES {
-                            todo!("max header size exceeded, send error response and close");
-                        }
-                        eprintln!("not enough request data yet");
-                    }
-                    Err(e) => {
-                        todo!("malformed request, send error and close");
-                    }
-                }
-
+    // TODO why the heck is it so hard to read into a vec's spare capacity???
+    let mut buf = Box::new([0; MAX_CONFIG_SIZE_BYTES]);
+    if body_start.len() < 4 {
+        match socket.read(buf.as_mut()).await {
+            // TODO Ok(0) ?
+            Ok(n) => {
+                body_start.extend_from_slice(&buf[..n]);
             }
+            Err(_) => { return send_response(socket, 400, "error bad read").await; }
         }
-        Ok(())
     }
+    // yeah its possible we don't get the full bit at this point but those clients can deal
+    if body_start.len() < 4 { return send_response(socket, 400, "error too short").await; }
 
-    fn register(&mut self, registry: &Registry) -> io::Result<()> {
-        let token_conn = Token::connection(self.index());
-        //let token_time = Token::timer(self.index());
-        registry.register(&mut self.connection, token_conn.into(), Interest::READABLE)?;
-        //registry.register(&mut self.resources.timer, token_time.into(), Interest::READABLE)?;
-        Ok(())
-    }
-
-    fn deregister(&mut self, registry: &Registry) {
-        let _ = registry.deregister(&mut self.connection);
-    }
-}
-
-
-struct RequestPool {
-    active: Vec<Option<Request>>,
-    inactive: Vec<RequestResources>,
-}
-
-impl RequestPool {
-    fn new(total_conn: usize) -> io::Result<Self> {
-        let mut ret = Self {
-            active: std::iter::repeat_with(|| None).take(total_conn).collect(),
-            inactive: Vec::with_capacity(total_conn),
-        };
-        for i in 0..total_conn {
-            ret.inactive.push(RequestResources::new(i)?);
-        }
-        Ok(ret)
-    }
-
-    fn pop(&mut self) -> Option<RequestResources> {
-        self.inactive.pop()
-    }
-
-    fn activate(&mut self, mut req: Request, registry: &Registry) -> io::Result<()> {
-        let index = req.index();
-        if self.active[index].is_some() {
-            panic!("no free slot here {index}");
-        }
-        req.register(registry)?;
-        self.active[index] = Some(req);
-        Ok(())
-    }
-
-    fn deactivate(&mut self, index: usize, registry: &Registry) {
-        if self.active[index].is_none() {
-            panic!("no active request here {index}");
-        }
-        let resources = self.active[index].take().unwrap().into_resources(registry);
-        self.inactive.push(resources);
-        // TODO unregister from registry?
-    }
-
-    fn connection_ready(&mut self, index: usize) -> io::Result<()> {
-        match self.active[index] {
-            None => { panic!("no active request here {index}"); },
-            Some(ref mut req) => {
-                req.connection_ready()
+    let config_size = {
+        let mut cursor = Cursor::new(&body_start);
+        match ReadBytesExt::read_u32::<LE>(&mut cursor) {
+            Ok(config_size) => {
+                let config_size = config_size as usize;
+                if config_size + 4 > content_length {
+                    return send_response(socket, 400, "bad config size").await;
+                }
+                if config_size + 4 > body_start.len() {
+                    return send_response(socket, 400, "should have gotten enough body already").await;
+                }
+                config_size
             }
+            _ => { return send_response(socket, 400, "got bad config size").await; }
         }
+    };
+    let archive_size = content_length - config_size;  // checked above
+
+    let api_v1_data: ApiV1Data = {
+        let config_buf = &body_start[4..4+config_size];
+        match serde_json::from_slice(config_buf) {
+            Ok(d) => d,
+            Err(_) => { return send_response(socket, 400, "bad api data").await; }
+        }
+    };
+    eprintln!("got api data {api_v1_data:?}");
+
+    return send_response(socket, 200, "wip").await;
+}
+
+async fn process_request(socket: TcpStream, method: &str, route: &str, content_length: Option<usize>, body_start: Vec<u8>) {
+    match (method, route, content_length) {
+        ("GET" , "/", _) => { return send_response(socket, 200, format!("hello world {content_length:?}").as_str()).await; }
+        ("POST", "/api/v1/i", Some(l)) => { return process_api_v1_i(socket, l, body_start).await; }
+        ("POST", "/api/v1/i", None)    => { return send_response(socket, 400, "bad content length").await; }
+        _  => { return send_response(socket, 404, "").await; }
     }
 }
 
-fn serve(config: Config) -> io::Result<()> {
-    let mut poll = Poll::new()?;
-    let mut events = Events::with_capacity(128);
-    const SERVER: mio::Token = mio::Token(0);
+// read headers until we have a full connection and maybe start of a body
+async fn process1(mut socket: TcpStream, addr: SocketAddr) {
+    eprintln!("connection from {addr}");
+    let mut offset = 0;
+    loop {
+        let mut buf = Box::new([0; MAX_HEADER_SIZE_BYTES]);
+        let n_read = match socket.read(&mut buf.as_mut()[offset..]).await {
+            Ok(n) => n,
+            Err(e) => { eprintln!("err {e}"); return send_response(socket, 400, "oh no err").await; },
+        };
+        let valid = &buf.as_ref()[..n_read + offset];
+        offset = valid.len();
+        let mut headers = [httparse::EMPTY_HEADER; MAX_HEADER_COUNT]; // Box?
+        let mut request = httparse::Request::new(&mut headers[..]);
+        match request.parse(valid) {
+            Ok(httparse::Status::Complete(body_start)) => {
+                let body = &valid[body_start..];
+                if request.version != Some(1) { return send_response(socket, 400, "version not 1.1").await; }
+                let content_length = match content_length(&request) {
+                    None => None,
+                    Some(l) if l as usize > MAX_BODY_SIZE => None,
+                    Some(l) => Some(l as usize),
+                };
+                match (request.method, request.path) {
+                    (None, _) | (_, None) => { return send_response(socket, 400, "missing method or path").await; }
+                    (Some(method), Some(route)) => {
+                        // do these behave like tail calls? I don't think so with the await
+                        return process_request(socket, method, route, content_length, body.to_vec()).await;
+                    }
+                }
+            }
+            Ok(httparse::Status::Partial) => {
+                if offset >= MAX_HEADER_SIZE_BYTES {
+                    return send_response(socket, 400, "too long").await;
+                }
+            }
+            Err(e) => { eprintln!("err {e}"); return send_response(socket, 400, "oh no err").await; },
+        }
+    }
+    //return send_response(socket, 200, "good job").await;
+}
 
-    let mut listener = TcpListener::bind(config.addr)?;
-    poll.registry().register(&mut listener, SERVER, Interest::READABLE)?;
 
-    let mut reqpool = RequestPool::new(config.total_conn)?;
+async fn serve(config: Config) {
+    // Bind the listener to the address
+    let listener = TcpListener::bind(config.addr).await.unwrap();
 
     loop {
-        poll.poll(&mut events, Some(Duration::from_millis(100)))?;
-
-        for event in events.iter() {
-            match event.token().into() {
-                Token::Server => loop {
-                    match reqpool.pop() {
-                        Some(resources) => {
-                            match listener.accept() {
-                                Ok(conn_addr) => {
-                                    // eprintln!("Got a connection from: {}", address);
-                                    reqpool.activate(resources.into_request(conn_addr), poll.registry())
-                                        .expect("couldnt register");
-                                },
-                                // A "would block error" is returned if the operation
-                                // is not ready, so we'll stop trying to accept
-                                // connections.
-                                Err(ref err) if would_block(err) => break,
-                                Err(err) => return Err(err),
-                            }
-                        }
-                        None => {
-                            eprintln!("backup, someone is knocking but not ready to answer");
-                            break;
-                        }
-                    }
-                },
-                Token::RequestConnection(i) => {
-                    match reqpool.connection_ready(i) {
-                        Ok(_) => {},
-                        Err(e) => {
-                            eprintln!("error on connection {e}");
-                            todo!();
-                        }
-                    }
-                },
-                _ => todo!()
-            }
-        }
+        // The second item contains the IP and port of the new connection.
+        let (socket, addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            process1(socket, addr).await;
+        });
     }
-}
-
-fn would_block(err: &io::Error) -> bool {
-    err.kind() == io::ErrorKind::WouldBlock
 }
 
 #[derive(Parser, Debug)]
@@ -332,7 +165,9 @@ struct Args {
     addr: String,
 }
 
-fn main() {
+
+#[tokio::main(flavor="current_thread")]
+async fn main() {
     let args = Args::parse();
     let addr = args.addr.parse().expect("bad socket addr");
     let config = Config {
@@ -340,7 +175,7 @@ fn main() {
         total_conn: 4,
         timeout_per_conn: Duration::from_millis(1000 * 5),
     };
-    serve(config).unwrap();
+    () = serve(config).await;
 }
 
 #[cfg(test)]
---
+++ peserver/src/testclient.rs
@@ -0,0 +1,51 @@
+use std::net::{TcpStream,SocketAddr};
+use clap::Parser;
+use std::io::{Read,Write};
+
+mod main;
+use main::ApiV1Data;
+
+fn escape_dump(input: &[u8]) {
+    let mut output = Vec::<u8>::with_capacity(1024);
+    for b in input.iter() {
+        for e in std::ascii::escape_default(*b) {
+            output.push(e);
+        }
+    }
+    let _ = std::io::stdout().write_all(output.as_slice()).unwrap();
+    println!("");
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, default_value = "127.0.0.1:8000")]
+    addr: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let addr: SocketAddr = args.addr.parse().expect("bad socket addr");
+
+    let mut conn = TcpStream::connect(addr).expect("couldn't connect");
+    let req_header = ApiV1Data {
+        image: "index.docker.io/busybox:1.32".to_string(),
+    };
+    let req_header_bytes = serde_json::to_vec(&req_header).expect("json ser");
+    let req_header_len: u32 = req_header_bytes.len().try_into().expect("should fit");
+    let l = req_header_bytes.len() + 4;
+    let data = format!("POST /api/v1/i HTTP/1.1\r\nContent-length: {l}\r\n\r\n");
+
+    eprintln!("writing content-length: {l} data size {}", req_header_bytes.len());
+
+    let mut full_data = Vec::new();
+    full_data.extend_from_slice(data.as_str().as_bytes());
+    full_data.extend_from_slice(&req_header_len.to_le_bytes());
+    full_data.extend_from_slice(&req_header_bytes);
+
+    conn.write_all(full_data.as_slice()).expect("bad write");
+
+    let mut buf = [0; 4096];
+    let n = conn.read(&mut buf).expect("bad read");
+    escape_dump(&buf[..n]);
+}
