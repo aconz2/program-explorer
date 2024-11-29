1fc53ef070b4ccf68443e1405d6025904b3953bd
Andrew Consroe <aconz2@gmail.com>
2024-11-29 15:39:58 -0600

    got an editor

--- pefrontend/src/index.tsx
+++ pefrontend/src/index.tsx
@@ -1,123 +1,119 @@
-import { render, Component } from 'preact';
+import { render, createRef, Component } from 'preact';
 import * as monaco from 'monaco-editor';
-import Editor from '@monaco-editor/react';
+//import Editor from '@monaco-editor/react';
 
-import preactLogo from './assets/preact.svg';
+
+// https://github.com/vitejs/vite/discussions/1791#discussioncomment-9281911
+import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
+import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
+import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
+import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+
+self.MonacoEnvironment = {
+  getWorker(_: any, label: string) {
+    if (label === "json") {
+      return new jsonWorker();
+    }
+    if (label === "css" || label === "scss" || label === "less") {
+      return new cssWorker();
+    }
+    if (label === "html" || label === "handlebars" || label === "razor") {
+      return new htmlWorker();
+    }
+    if (label === "typescript" || label === "javascript") {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
 import './style.css';
-
-//export function App() {
-//	return (
-//		<div>
-//			<a href="https://preactjs.com" target="_blank">
-//				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-//			</a>
-//			<h1>Get Started building Vite-powered Preact Apps </h1>
-//			<section>
-//				<Resource
-//					title="Learn Preact"
-//					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-//					href="https://preactjs.com/tutorial"
-//				/>
-//				<Resource
-//					title="Differences to React"
-//					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-//					href="https://preactjs.com/guide/v10/differences-to-react"
-//				/>
-//				<Resource
-//					title="Learn Vite"
-//					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-//					href="https://vitejs.dev"
-//				/>
-//			</section>
-//		</div>
-//	);
-//}
-//
-//function Resource(props) {
-//	return (
-//		<a href={props.href} target="_blank" class="resource">
-//			<h2>{props.title}</h2>
-//			<p>{props.description}</p>
-//		</a>
-//	);
-//}
-
-//export function App() {
-//    render (
-//        <div>
-//            <div id="editor"></div>
-//        </div>
-//    );
-//}
 
 type File = {
     name: string,
-    language: string,
-    value: string,
+    data: string,
 };
 
-type Files = {[name: string]: File};
+function getOrCeateFile(path, text) {
+    let uri = monaco.Uri.file(path);
+    return monaco.editor.getModel(uri) || monaco.editor.createModel(text, undefined, uri);
+}
 
-const files: Files = {
-  'script.js': {
-    name: 'script.js',
-    language: 'javascript',
-    value: 'let a = 10;',
-  },
-  'style.css': {
-    name: 'style.css',
-    language: 'css',
-    value: 'body { border: 10px; }',
-  },
-  'index.html': {
-    name: 'index.html',
-    language: 'html',
-    value: '<div>hi</div>',
-  },
-};
-
-class App extends Component {
-    state: {
-        file: File,
-        files: Files,
+class Editor extends Component {
+    //model: monaco.ITextModel; // todo figure out how to get this
+    ref = createRef();
+    editor: any;
+    state = {
+        models: [],
     };
 
-    constructor({files: Files}) {
+    constructor({initialFiles}) {
         super();
-        console.log(files);
-        this.state = {
-            files: files,
-            file: files['script.js'],
-        };
+        this.state.models = initialFiles.map(({name, data}) => getOrCeateFile(name, data));
     }
 
-    setFileName(s) {
-        this.setState({file: files[s]});
+    componentDidMount() {
+        this.editor = monaco.editor.create(this.ref.current, {
+            placeholder: 'hello enter your text here',
+        });
+    }
+
+    editFile(uri) {
+        this.editor.updateOptions({placeholder: ''});
+        this.editor.setModel(monaco.editor.getModel(uri));
     }
 
     // this.props, this.state
-    render({}, {file, fileName}) {
+    render({}, {models}) {
+        let tabs = models.map((model) => {
+            console.log(model.uri);
+            return (<button key={model.id} onClick={() => this.editFile(model.uri)}>{model.uri.path}</button>);
+        });
         return <div>
-          <button disabled={file.name === 'script.js'} onClick={() => this.setFileName('script.js')}>
-            script.js
-          </button>
-          <button disabled={file.name === 'style.css'} onClick={() => this.setFileName('style.css')}>
-            style.css
-          </button>
-          <button disabled={file.name === 'index.html'} onClick={() => this.setFileName('index.html')}>
-            index.html
-          </button>
+            {tabs}
+            <div class="editorContainer" ref={this.ref}></div>
+        </div>;
+    }
+}
+
+class App extends Component {
+    constructor({initialFiles}) {
+        super();
+    }
+
+    // this.props, this.state
+    render({initialFiles}, {}) {
+        return <div>
           <Editor
-            height="80vh"
-            theme="vs-dark"
-            path={file.name}
-            defaultLanguage={file.language}
-            defaultValue={file.value}
+            initialFiles={initialFiles}
           />
         </div>
     }
 }
 
+const files: File[] = [
+  {
+    name: 'script.js',
+    data: 'let a = 10;',
+  },
+  {
+    name: 'style.css',
+    data: 'body { border: 10px; }',
+  },
+  {
+    name: 'index.html',
+    data: '<div>hi</div>',
+  },
+];
+
+
 // TODO something about the args going in not being compat with the Component type signature
-// @ts-ignore
-render(<App files={files} />, document.getElementById('app'));
+render(<App initialFiles={files} />, document.getElementById('app'));
+//<Editor
+//  height="80vh"
+//  theme="vs-dark"
+//  path={file.name}
+//  defaultLanguage={file.language}
+//  defaultValue={file.value}
+///>
