19402f83baae1de9bbb17832423364bc6fded8b5
Andrew Consroe <aconz2@gmail.com>
2025-02-10 16:08:19 -0600

    boo silly mistake, was corrupting the cpio archive

--- scripts/build-initramfs.sh
+++ scripts/build-initramfs.sh
@@ -5,7 +5,7 @@ set -e
 profile=${1:-debug}
 crun=${CRUN:-vendor/crun-1.15-linux-amd64}
 
-echo "using profile=$profile crun=$crun"
+echo "using profile=$profile crun=$crun" 1>2
 
 # TODO figure out a better way to refer to this tool (or vendor or rewrite it)
 # sed with ! separator to avoid problem of var subs with / in it... bad bad bad
