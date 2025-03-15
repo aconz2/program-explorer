614f639b324e218efb0825bd975373306d72cc03
Andrew Consroe <aconz2@gmail.com>
2025-03-14 19:02:28 -0500

    add privacy policy

--- pefrontend/vite.config.ts
+++ pefrontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import { dirname , resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [preact()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname , 'index.html'),
+                privacy: resolve(__dirname , 'privacy.html'),
+            },
+        },
+    },
 });
